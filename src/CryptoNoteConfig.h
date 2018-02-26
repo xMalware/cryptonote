a8ab1301c32ae05e200fd4adf1b26d00ac2f70dd
xMalware <xmalware2@gmail.com>
2018-02-26 06:16:27 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "149.202.86.66:17236",
+  "213.32.7.171:17236",
 };
 
 struct CheckpointData {
